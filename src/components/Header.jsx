740642ecf4c4cfd7e5a2332f0c98f2c0733fe33e
Hugo Coutinho <hugocoutinho96@outlook.com>
2025-02-02 10:32:31 -0300

    Atualizações na responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
 import styles from "./Header.module.css"
-import {Code} from "phosphor-react"
+import {Code, List} from "phosphor-react"
 
 export function Header(){
 
+    function toggleMenu() {
+        var menu = document.getElementById("mobileMenu");
+        if (menu.style.maxHeight === "0px" || menu.style.maxHeight === "") {
+            menu.style.maxHeight = "300px"; // Ajuste conforme necessário
+        } else {
+            menu.style.maxHeight = "0px";
+        }
+    }
+
     const scrollToTop = (e) => {
         let position = 0
-        if(e == 1)
-            position = 0
-        else if(e == 2)
-            position = 780
-        else
-            position = 1200
+
+        switch(e){
+            case 1: position = 0; break;
+            case 2: position = 780; break;
+            case 3: position = 1200; break;
+            case 4: position = 0; break;
+            case 5: position = 640; break;
+            case 6: position = 1800; break;
+        }
 
         window.scrollTo({
         top: position,
@@ -29,6 +41,14 @@ export function Header(){
                 <a href="#" onClick={e => scrollToTop(2)}>Portfólio</a>
                 <a href="#" onClick={e => scrollToTop(3)}>Contato</a>
             </nav>
+
+            <div className={styles['mobile-menu']} id="mobileMenu">
+                <a href="#" onClick={e => scrollToTop(4)}>Sobre mim</a>
+                <a href="#" onClick={e => scrollToTop(5)}>Portfólio</a>
+                <a href="#" onClick={e => scrollToTop(6)}>Contato</a>
+            </div>
+
+            <button className={styles['menu-btn']} onClick={toggleMenu}><List size={32} /></button>
         </header>
     )
 }
